f0634b88521ead0355f21d26da93d0154fa7ea87
Kyle Warren <kyle@timehop.com>
2012-08-11 14:16:15 -0400

    began adding annotations

--- js/lossures.js
+++ js/lossures.js
@@ -9,13 +9,15 @@ var LS = (function () {
 
     vid.loop = true;
     vid.playbackRate = 1;
-    vid.play();
 
     $('#video-pause').on('click', function () {
       vid.pause();
       container.className = 'minimize';
       $('#video-pause').hide();
       $('#video-play').show();
+      setTimeout(function () {
+        document.getElementById('ls-pause-1').classList.add('fade-in');
+      }, 1000);
     });
 
     $('#video-play').on('click', function () {
@@ -25,6 +27,27 @@ var LS = (function () {
       vid.play();
     });
 
+    vid.playbackRate = 0.1;
+    //vid.play();
+    //
+    $('.ls-anno-img').toggle(function () {
+      var $el = $(this).addClass('large'),
+          $w = $(window),
+          $img = $('img', $el),
+          top = ($w.height() - $img.height()) / 2,
+          left = ($w.width() - $img.width()) / 2;
+
+      if (top < 0) {
+        top = 0;
+      }
+      if (left < 0) {
+        left = 0;
+      }
+
+      $img.css({top: top, left: left});
+    }, function () {
+      $(this).removeClass('large');
+    });
   };
 
   return me;
